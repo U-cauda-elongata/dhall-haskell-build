5ba9a99345ed791018221d4a7b214a25ed25f109
Yu Onaga <ursus.cauda.elongata@gmail.com>
2021-04-24 18:59:11 +0900

    Build individual packages parallely

--- build.sh
+++ build.sh
@@ -4,6 +4,8 @@ set -o errexit -o xtrace
 
 main() {
     local target="${1}"
+    shift
+    local packages=( "${@}" )
 
     local platform=
     case "${target}" in
@@ -38,27 +40,46 @@ main() {
         --volume="$(pwd)/.stack":/.stack \
         --workdir=/build \
         ghcr.io/u-cauda-elongata/haskell:latest \
-        stack build --verbose --system-ghc --no-install-ghc --copy-bins --local-bin-path=bin
+        stack build "${packages[@]}" --verbose --system-ghc --no-install-ghc --copy-bins --local-bin-path=bin
 
-    mkdir -p share/man/man1
-    cp dhall-haskell/dhall/man/dhall.1 share/man/man1/
-    cp dhall-haskell/dhall-docs/src/Dhall/data/man/dhall-docs.1 share/man/man1/
+    if [ "${#packages[@]}" -eq 0 ]; then
+        packages=(
+            dhall
+            dhall-json
+            dhall-bash
+            dhall-lsp-server
+            dhall-nix
+            dhall-openapi
+            dhall-yaml
+            dhall-docs
+        )
+    fi
 
-    get_cabal_version() {
-        grep -iPo '(?<=^Version: ).*' "dhall-haskell/$1/$1.cabal" | sed 's/^ *//'
-    }
-    mk_release_name() {
-        echo "dist/$1-$(get_cabal_version "$1")-$2.tar.bz2"
-    }
+    for package in "${packages[@]}"; do
+        local assets=
+        case "${package}" in
+        dhall)
+            mkdir -p share/man/man1
+            cp dhall-haskell/dhall/man/dhall.1 share/man/man1/
+            assets=( bin/dhall share/man/man1/dhall.1 )
+            ;;
+        dhall-docs)
+            mkdir -p share/man/man1
+            cp dhall-haskell/dhall-docs/src/Dhall/data/man/dhall-docs.1 share/man/man1/
+            assets=( bin/dhall-docs share/man/man1/dhall-docs.1 )
+            ;;
+        *)
+            IFS=$'\n'
+            # shellcheck disable=SC2207
+            assets=( $(grep -iPo '(?<=^Executable\s).*' "dhall-haskell/${package}/${package}.cabal" | sed 's!^ *!bin/!') )
+            ;;
+        esac
 
-    tar -jcvf "$(mk_release_name dhall "${target}")" bin/dhall share/man/man1/dhall.1
-    tar -jcvf "$(mk_release_name dhall-json "${target}")" bin/dhall-to-json bin/dhall-to-yaml bin/json-to-dhall
-    tar -jcvf "$(mk_release_name dhall-bash "${target}")" bin/dhall-to-bash
-    tar -jcvf "$(mk_release_name dhall-lsp-server "${target}")" bin/dhall-lsp-server
-    tar -jcvf "$(mk_release_name dhall-nix "${target}")" bin/dhall-to-nix
-    tar -jcvf "$(mk_release_name dhall-openapi "${target}")" bin/openapi-to-dhall
-    tar -jcvf "$(mk_release_name dhall-yaml "${target}")" bin/dhall-to-yaml-ng bin/yaml-to-dhall
-    tar -jcvf "$(mk_release_name dhall-docs "${target}")" bin/dhall-docs share/man/man1/dhall-docs.1
+        local version=
+        version="$(grep -iPo '(?<=^Version: ).*' "dhall-haskell/${package}/${package}.cabal" | sed 's/^ *//g')"
+
+        tar -jcvf "dist/${package}-${version}-${target}.tar.bz2" "${assets[@]}"
+    done
 }
 
 main "${@}"
